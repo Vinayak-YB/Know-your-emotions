b5016d1d17f1ed1341167bfdf4fd609667f1ed5b
Vinayak Y B <vinayak.bhustali@gmail.com>
2021-09-15 12:16:54 +0530

    Changes are made

---
+++ app.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import "./styles.css";
+var emojiDictionary = {
+  "😊": "Smiling",
+  "😂": "Laughing",
+  "😢": "Crying",
+  "❤": "Love",
+  "😉": "Winking",
+  "😒": "Annoyed",
+  "😁": "Happy"
+};
+var emojisWeKnow = Object.keys(emojiDictionary);
+export default function App() {
+  var [meaning, setMeaning] = useState("");
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+    if (meaning === undefined) {
+      meaning = "We don't have this in out Database";
+    }
+    setMeaning(meaning);
+  }
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+  return (
+    <div className="App">
+      <h1> Inside Out! </h1>
+      <input onChange={emojiInputHandler}></input>
+      <h2> {meaning} </h2>
+      <h3>Emojis we know </h3>
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {" "}
+            {emoji}{" "}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
+import React, { useState } from "react";
+import "./styles.css";
+var emojiDictionary = {
+  "😊": "Smiling",
+  "😂": "Laughing",
+  "😢": "Crying",
+  "❤": "Love",
+  "😉": "Winking",
+  "😒": "Annoyed",
+  "😁": "Happy"
+};
+var emojisWeKnow = Object.keys(emojiDictionary);
+export default function App() {
+  var [meaning, setMeaning] = useState("");
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+    if (meaning === undefined) {
+      meaning = "We don't have this in out Database";
+    }
+    setMeaning(meaning);
+  }
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+  return (
+    <div className="App">
+      <h1> Inside Out! </h1>
+      <input onChange={emojiInputHandler}></input>
+      <h2> {meaning} </h2>
+      <h3>Emojis we know </h3>
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {" "}
+            {emoji}{" "}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
